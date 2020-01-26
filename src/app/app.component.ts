4347ca4a71af3c3a52acfe68699ec88d030a03ef
noctuh <yisho92@gmail.com>
2020-01-27 00:59:53 +0100

    init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less'],
+})
+export class AppComponent {
+  title = 'poehelper';
+
+  constructor() {
+    // var options: RConnectionOptions = {};
+    // options.user = 'admin';
+    // options.password = '';
+    // options.host = '157.230.17.78';
+    // options.port = 28015;
+    // options.db = 'test';
+    // options.buffer = 2;
+    // options.max = 5;
+    // console.log(options);
+    // var connection = null;
+    // const conn = r.connect(options);
+    // conn.then(res => {
+    //   console.log('OK');
+    // }, error => {
+    //   console.log('ERROR');
+    //   console.log(error);
+    // })
+    // console.log(conn);
+  }
+}
---
+++ src/app/config.ts
@@ -0,0 +1,14 @@
+export default {
+//     host: process.env.RETHINKDB_HOST || 'localhost',
+//     port: parseInt(process.env.RETHINKDB_PORT || '', 10) || 28015,
+//     user: 'admin',
+//     password: '',
+//     buffer: 2,
+//     max: 5,
+//     fake_server: {
+//       host: process.env.RETHINKDB_HOST || 'localhost',
+//       port: parseInt(process.env.RETHINKDB_PORT || '', 10) + 1 || 28016
+//     },
+//     discovery: false,
+//     silent: true
+};
