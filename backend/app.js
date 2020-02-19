8a6f5cd57fdcd5ac560eef215eca736cb1a2e6be
Lukas Schneider <lukas_schneider1992@gmx.de>
2020-02-19 17:06:20 +0100

    added first rethink db logic

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,33 @@ const settingsRoutes = require('./routes/user-settings');
 
 const app = express();
 
+const r = require('rethinkdb');
+
+const setupDatabase = (conn, databaseName, tableNames) => {
+  if (!_.isNil(tableName) && _.isString(tableName) && !_.isNil(conn)) {
+    r.tableList().run(conn).then(function(tableNames) {
+      if (_.includes(tableNames, tableName)) {
+        return r.tableCreate(tableName).run(conn);
+      } else {
+        return;
+        }
+    });
+  }
+};
+
+r.connect({ host: '104.248.18.211', port: 28015, user: 'admin', password: '' }, function(err, conn) {
+  if(err) throw err;
+  const databaseName = 'poehelper';
+  const tableNames = ['items', 'stashes'];
+  setupDatabase(conn, databaseName, tableNames).then(() => {
+    r.db('poehelper').table('items').insert({ name: 'Default One Hand Axe' }).run(conn, function(err, res)
+    {
+      if(err) throw err;
+      console.log(res);
+    });
+  })
+});
+
 mongoose.connect('mongodb+srv://lukas:' + process.env.MONGO_ATLAS_PW + '@cluster0-gsvo3.mongodb.net/node-angular?retryWrites=true&w=majority', { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to DB!');
--- backend/models/user-settings.js
+++ backend/models/user-settings.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSettingsSchema = mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    poeAccountName: { type: string }
+    poeAccountName: { type: String }
 });
 
 module.exports = mongoose.model('UserSettings', userSettingsSchema);
